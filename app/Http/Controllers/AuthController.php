7cf247a36a1be753c0bdc9283ec4c155974990a4
syawalihbb <syawalihbb@gmail.com>
2023-12-31 20:07:10 +0700

    menambahkan halaman login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function login()
+    {
+        return view ('auth.login');
+    }
+
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if(Auth::attempt($credentials)) {
+            return redirect('posts');
+        } else {
+            return redirect('login')->with('error_message', 'Failed To Login');
+        }
+    }
+
+}
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title', 'Login')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="card col-md-4">
+            <div class="card-body">
+                <h1 class="text-center">Login</h1>
+
+                @if(session()->has('error_message'))
+                    <div class="alert alert-danger">
+                        {{ session()->get('error_message') }}
+                    </div>
+                @endif
+
+                <form action="{{ url('login') }}" method="POST" class="form-control">
+                    @csrf
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" name="email" id="email" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" name="password" id="password" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <button type="submit" class="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
